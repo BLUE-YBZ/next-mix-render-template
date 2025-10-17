3e31daedf53edad0d7f88ed364087570895cdd5b
melodyyyang <melodyyyang@tencent.com>
2025-10-17 11:00:24 +0800

    feat: 更新文本v1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Features from '@/components/Features'
 export default function Home() {
   return (
     <PageLayout>
-      dsdsdsdserrrrtttv9
+      dsdsdsdserrrrtttv1.0
       <Hero />
       <Features />
     </PageLayout>
